323dd36d4d13fed107bc3d81f49301e8321e5deb
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-02-12 21:14:01 -0300

    criação- function/calculate - finalizando integração- API- ExchangrRate

--- js/conversor.js
+++ js/conversor.js
@@ -37,10 +37,14 @@ function selection(){
     let currency = {
         "BRL": "REAL",
         "USD": "DOLAR",
-        "CBP": "LIBRA",
+        "GBP": "LIBRA",
         "EUR": "EURO",
         "ARS": "PESO ARGENTINO",
-        "CLP": "PESO CHILENO"
+        "CLP": "PESO CHILENO",
+        "BTC": "BITCOIN",
+        "CNY": "RENMINBI",
+        "JPY": "YEN",
+        "RUB": "RUBLE"
     }
     
 
@@ -67,39 +71,22 @@ function selection(){
     }
 
         async function getURL(url){
-        return(await fetch(url)).jason();
+        return(await fetch(url)).json();
     }
 
     function getInfoSelect (select){
-        return select.options[select.selectedIndex]
+        return select.options[select.selectedIndex].text;
     }
 
    async function calculate(){
         let from = selectOne.value;
         let to = selectTwo.value;
 
-        let{rates }= await getURL(`"https://api.apilayer.com/fixer/latest?symbols=BRL%2C%20EUR%2C%20CLP%2C%20ARS%2C%20GBP%2C%20USD&base=USD"`);
+        let{ rates } = await getURL(`https://api.exchangerate-api.com/v4/latest/${from}`);
 
         let rate = rates[to];
 
         inputTwo.value = (inputOne.value * rate).toFixed(2);
     }
 
-
-
-
-
-    // var myHeaders = new Headers();
-    // myHeaders.append("apikey", "glz3IJUa9Cr9RJjUwwJbYgkGTU7mu8VT");
-    
-    // var requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow',
-    //   headers: myHeaders
-    // };
-    
-    // fetch("https://api.apilayer.com/fixer/convert?to=usd&from=brl&amount=1", requestOptions)
-    //   .then(response => response.text())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
-
+   
