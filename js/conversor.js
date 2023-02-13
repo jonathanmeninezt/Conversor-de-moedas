afcbb328e9ef14d0295720a59ecb07e46c9e16d8
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-02-13 11:34:26 -0300

    limpeza de códigos em comentário- js/conversor.js

--- js/conversor.js
+++ js/conversor.js
@@ -1,19 +1,4 @@
-// var myHeaders = new Headers();
-// myHeaders.append("apikey", "glz3IJUa9Cr9RJjUwwJbYgkGTU7mu8VT");
 
-// var requestOptions = {
-//   method: 'GET',
-//   redirect: 'follow',
-//   headers: myHeaders
-// };
-
-// fetch("https://api.apilayer.com/fixer/latest?symbols=BRL%2C%20EUR%2C%20CLP%2C%20ARS%2C%20GBP%2C%20USD&base=USD", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-
-// var endpoint = "https://api.apilayer.com/fixer/latest?symbols=BRL%2C%20EUR%2C%20CLP%2C%20ARS%2C%20GBP%2C%20USD&base=USD";
 
 const selectOne = document.getElementById('from_currency');
 const selectTwo = document.getElementById('to_currency');
